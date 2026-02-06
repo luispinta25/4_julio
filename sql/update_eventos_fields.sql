0c7624c39df79d02ad7a53929bd6e60ff8f09357
luispinta25 <contacto@luispinta.com>
2026-02-06 14:44:27 -0500

    Delete sql directory

--- sql/update_eventos_fields.sql
+++
@@ -1,5 +0,0 @@
--- Añadir columnas para lugar y punto de encuentro en eventos
-ALTER TABLE public.unoric_eventos 
-ADD COLUMN IF NOT EXISTS lugar text,
-ADD COLUMN IF NOT EXISTS punto_encuentro text,
-ADD COLUMN IF NOT EXISTS alcance_detalle text;
